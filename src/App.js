23697e776e51e9b0300b9e1383b38bfd38f7bc9c
Alessandra Longaretti <tauan@Tauans-iMac.local>
2019-11-19 23:39:34 -0500

    created folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+import AstronomyContainer from './components/AstronomyContainer';
 
 function App() {
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+       
+      </p> 
+      <AstronomyContainer />
+    
     </div>
   );
 }
